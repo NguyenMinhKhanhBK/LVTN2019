9fca0dd3e63bc4943d911fca86668828e01c514f
Nguyen Minh Khanh <nmkhanhbk@gmail.com>
2019-02-15 12:53:54 +0700

    Completed basic objects and elements, remove some advanced controls

--- public/js/myScript.js
+++ public/js/myScript.js
@@ -1,4 +1,8 @@
-// Document ready
+/*
+***********************************************************************************************
+                                Document ready 
+***********************************************************************************************
+*/
 $(document).ready(function () {
   $('[data-toggle="tooltip"]').tooltip();
 
@@ -9,6 +13,11 @@ $(document).ready(function () {
   });
 });
 
+/*
+***********************************************************************************************
+                                Global variable
+***********************************************************************************************
+*/
 //SVG global variable
 const draw = SVG('mainPage1');
 const shapes = [];
@@ -28,6 +37,12 @@ const defaultLineOption = {
   'stroke-width': 5,
   'stroke-linecap': 'round'
 };
+
+/*
+***********************************************************************************************
+                                Create object functions 
+***********************************************************************************************
+*/
 
 //startDraw function: Start drawing object depending on the parameter
 //Input: shape (except POLYGON)
@@ -127,14 +142,6 @@ var drawPoly = function () {
   });
 }
 
-//stopDraw function: Stop all draw action
-var stopDraw = function () {
-  draw.off();
-  $('#mainPage1').off('mousedown', imageMouseDownEventHandler);
-  $('#mainPage1').off('mousedown', textMouseDownEventHandler);
-  $('#mainPage1').off('mousedown', displayValueMouseDownEventHandler);
-  $('#mainPage1').off('mousedown', buttonMouseDownEventHandler);
-}
 
 //Add new image
 function addNewImage() {
@@ -159,6 +166,69 @@ function addNewButton() {
   stopDraw();
   $('#mainPage1').on('mousedown', buttonMouseDownEventHandler);
 }
+
+//Add new switch
+function addNewSwitch() {
+  stopDraw();
+  $('#mainPage1').on('mousedown', switchMouseDownEventHandler);
+}
+
+//Add new input
+function addNewInput() {
+  stopDraw();
+  $('#mainPage1').on('mousedown', inputMouseDownEventHandler);
+}
+
+//Add new checkbox
+function addNewCheckbox() {
+  stopDraw();
+  $('#mainPage1').on('mousedown', checkboxMouseDownEventHandler);
+}
+
+//Add new slider
+function addNewSlider() {
+  stopDraw();
+  $('#mainPage1').on('mousedown', sliderMouseDownEventHandler);
+}
+
+//Add new process bar
+function addNewProcessbar() {
+  stopDraw();
+  $('#mainPage1').on('mousedown', processbarMouseDownEventHandler);
+}
+
+//Add new symbol set
+function addNewSymbolSet() {
+  stopDraw();
+  $('#mainPage1').on('mousedown', symbolsetMouseDownEventHandler);
+}
+
+/*
+***********************************************************************************************
+                                Stop drawing function 
+***********************************************************************************************
+*/
+
+//stopDraw function: Stop all draw action
+var stopDraw = function () {
+  draw.off();
+  $('#mainPage1').off('mousedown', imageMouseDownEventHandler);
+  $('#mainPage1').off('mousedown', textMouseDownEventHandler);
+  $('#mainPage1').off('mousedown', displayValueMouseDownEventHandler);
+  $('#mainPage1').off('mousedown', buttonMouseDownEventHandler);
+  $('#mainPage1').off('mousedown', switchMouseDownEventHandler);
+  $('#mainPage1').off('mousedown', inputMouseDownEventHandler);
+  $('#mainPage1').off('mousedown', checkboxMouseDownEventHandler);
+  $('#mainPage1').off('mousedown', sliderMouseDownEventHandler);
+  $('#mainPage1').off('mousedown', processbarMouseDownEventHandler);
+  $('#mainPage1').off('mousedown', symbolsetMouseDownEventHandler);
+}
+
+/*
+***********************************************************************************************
+                                Event handlers
+***********************************************************************************************
+*/
 
 //Keydown ENTER event handler: To stop drawing polygon
 function keyEnterDownHandler(e) {
@@ -226,7 +296,7 @@ function textMouseDownEventHandler(event) {
   para.style.position = 'absolute';
   para.style.top = top;
   para.style.left = left;
-  
+
 
   //Image mouse events
   $(para).on('mouseover', function (event) {
@@ -264,7 +334,7 @@ function displayValueMouseDownEventHandler(event) {
   para.style.position = 'absolute';
   para.style.top = top;
   para.style.left = left;
-  
+
 
   //Image mouse events
   $(para).on('mouseover', function (event) {
@@ -302,7 +372,7 @@ function buttonMouseDownEventHandler(event) {
   btn.style.position = 'absolute';
   btn.style.top = top;
   btn.style.left = left;
-  
+
 
   //Image mouse events
   $(btn).on('mouseover', function (event) {
@@ -321,5 +391,311 @@ function buttonMouseDownEventHandler(event) {
   console.log(shapes);
 }
 
+//Switch mouse down event handler: To create new switch
+function switchMouseDownEventHandler(event) {
+  var leftOffset = document.getElementById('mainPage1').getBoundingClientRect().left;
+  var topOffset = document.getElementById('mainPage1').getBoundingClientRect().top;
+
+  var left = event.pageX - leftOffset + 'px';
+  var top = event.pageY - topOffset + 'px';
+
+  //Declare new paragrap
+  var sw = document.createElement('label');
+  sw.className = 'switch';
+
+  var inputsw = document.createElement('input');
+  inputsw.setAttribute('type', 'checkbox');
+  inputsw.className = 'primary';
+
+  var spansw = document.createElement('span');
+  spansw.className = 'slider round';
+
+  sw.appendChild(inputsw);
+  sw.appendChild(spansw);
+
+  sw.id = 'switch' + index;
+
+  //Image css style
+  sw.style.position = 'absolute';
+  sw.style.top = top;
+  sw.style.left = left;
 
 
+  //Image mouse events
+  $(sw).on('mouseover', function (event) {
+    event.target.style.opacity = 0.65;
+    event.target.style.cursor = 'move';
+  });
+  //Subscribe mouseout event for each polygon
+  $(sw).on('mouseout', function (event) {
+    event.target.style.opacity = 1;
+  });
+
+  $('#mainPage1').append(sw);
+  shapes[index] = sw;
+  index++;
+
+  console.log(shapes);
+}
+
+// function switchMouseDownEventHandler(event) {
+//   var leftOffset = document.getElementById('mainPage1').getBoundingClientRect().left;
+//   var topOffset = document.getElementById('mainPage1').getBoundingClientRect().top;
+
+//   var left = event.pageX - leftOffset + 'px';
+//   var top = event.pageY - topOffset + 'px';
+
+//   //Declare new paragrap
+//   var sw = document.createElement('div');
+//   sw.className = 'custom-control custom-switch';
+
+//   var input = document.createElement('input');
+//   input.setAttribute('type', 'checkbox');
+//   input.className = 'custom-control-input';
+//   input.id = 'input' + index;
+
+//   var label = document.createElement('label');
+//   label.className = 'custom-control-label';
+//   label.htmlFor = 'input'+index;
+//   label.innerText = 'Switch';
+
+//   sw.appendChild(input);
+//   sw.appendChild(label);
+
+//   sw.id = 'switch' + index;
+
+//   //Image css style
+//   sw.style.position = 'absolute';
+//   sw.style.top = top;
+//   sw.style.left = left;
+
+
+//   //Image mouse events
+//   $(sw).on('mouseover', function (event) {
+//     event.target.style.opacity = 0.65;
+//     event.target.style.cursor = 'move';
+//   });
+//   //Subscribe mouseout event for each polygon
+//   $(sw).on('mouseout', function (event) {
+//     event.target.style.opacity = 1;
+//   });
+
+//   $('#mainPage1').append(sw);
+//   shapes[index] = sw;
+//   index++;
+
+//   console.log(shapes);
+// }
+
+//Input mouse down event handler: To create new input
+
+function inputMouseDownEventHandler(event) {
+  var leftOffset = document.getElementById('mainPage1').getBoundingClientRect().left;
+  var topOffset = document.getElementById('mainPage1').getBoundingClientRect().top;
+
+  var left = event.pageX - leftOffset + 'px';
+  var top = event.pageY - topOffset + 'px';
+
+  //Declare new paragrap
+  var input = document.createElement('input');
+  input.type = 'text';
+  input.id = 'input' + index;
+  input.placeholder = 'Add text ...';
+
+  //Image css style
+  input.className = 'form-control';
+  input.style.width = '200px';
+  input.style.position = 'absolute';
+  input.style.top = top;
+  input.style.left = left;
+
+
+  //Image mouse events
+  $(input).on('mouseover', function (event) {
+    event.target.style.opacity = 0.4;
+    event.target.style.cursor = 'move';
+  });
+  //Subscribe mouseout event for each polygon
+  $(input).on('mouseout', function (event) {
+    event.target.style.opacity = 1;
+  });
+
+  $('#mainPage1').append(input);
+  shapes[index] = input;
+  index++;
+
+  console.log(shapes);
+}
+
+//Checkbox mouse down event handler: To create new Checkbox
+function checkboxMouseDownEventHandler(event) {
+  var leftOffset = document.getElementById('mainPage1').getBoundingClientRect().left;
+  var topOffset = document.getElementById('mainPage1').getBoundingClientRect().top;
+
+  var left = event.pageX - leftOffset + 'px';
+  var top = event.pageY - topOffset + 'px';
+
+  //Declare new paragrap
+  var checkbox = document.createElement('div');
+  checkbox.className = 'custom-control custom-checkbox';
+  checkbox.id = 'checkbox' + index;
+
+  var cbInput = document.createElement('input');
+  cbInput.type = 'checkbox';
+  cbInput.className = 'custom-control-input';
+  cbInput.id = 'cbInput'+index;
+
+  var cbLabel = document.createElement('label');
+  cbLabel.className = 'custom-control-label';
+  cbLabel.htmlFor = 'cbInput'+index;
+  cbLabel.innerText = 'Checkbox';
+
+  checkbox.appendChild(cbInput);
+  checkbox.appendChild(cbLabel);
+
+
+  //Image css style
+  checkbox.style.position = 'absolute';
+  checkbox.style.top = top;
+  checkbox.style.left = left;
+
+
+  //Image mouse events
+  $(checkbox).on('mouseover', function (event) {
+    event.target.style.opacity = 0.4;
+    event.target.style.cursor = 'move';
+  });
+  //Subscribe mouseout event for each polygon
+  $(checkbox).on('mouseout', function (event) {
+    event.target.style.opacity = 1;
+  });
+
+  $('#mainPage1').append(checkbox);
+  shapes[index] = checkbox;
+  index++;
+
+  console.log(shapes);
+}
+
+//Slider mouse down event handler: To create new Checkbox
+function sliderMouseDownEventHandler(event) {
+  var leftOffset = document.getElementById('mainPage1').getBoundingClientRect().left;
+  var topOffset = document.getElementById('mainPage1').getBoundingClientRect().top;
+
+  var left = event.pageX - leftOffset + 'px';
+  var top = event.pageY - topOffset + 'px';
+
+  //Declare new paragrap
+  var slider = document.createElement('input');
+  slider.type = 'range';
+  slider.className = 'custom-range';
+  slider.id = 'slider' + index;
+
+  //Image css style
+  slider.style.position = 'absolute';
+  slider.style.top = top;
+  slider.style.left = left;
+  slider.style.width = '500px';
+
+
+  //Image mouse events
+  $(slider).on('mouseover', function (event) {
+    event.target.style.opacity = 0.4;
+    event.target.style.cursor = 'move';
+  });
+  //Subscribe mouseout event for each polygon
+  $(slider).on('mouseout', function (event) {
+    event.target.style.opacity = 1;
+  });
+
+  $('#mainPage1').append(slider);
+  shapes[index] = slider;
+  index++;
+
+  console.log(shapes);
+}
+
+//Process bar mouse down event handler: To create new Checkbox
+function processbarMouseDownEventHandler(event) {
+  var leftOffset = document.getElementById('mainPage1').getBoundingClientRect().left;
+  var topOffset = document.getElementById('mainPage1').getBoundingClientRect().top;
+
+  var left = event.pageX - leftOffset + 'px';
+  var top = event.pageY - topOffset + 'px';
+
+  //Declare new paragrap
+  var progressbar = document.createElement('div');
+  progressbar.className = 'progress';
+  progressbar.id = 'progressbar' + index;
+
+  var bar = document.createElement('div');
+  bar.className = 'progress-bar';
+  bar.style.width = '70%';
+  //bar.style.height = '20px';
+  bar.innerText = '70%';
+
+
+  progressbar.appendChild(bar);
+
+  //Image css style
+  progressbar.style.position = 'absolute';
+  progressbar.style.top = top;
+  progressbar.style.left = left;
+  progressbar.style.width = '600px';
+  //progressbar.style.height = '20px';
+
+
+  //Image mouse events
+  $(progressbar).on('mouseover', function (event) {
+    event.target.style.opacity = 0.4;
+    event.target.style.cursor = 'move';
+  });
+  //Subscribe mouseout event for each polygon
+  $(progressbar).on('mouseout', function (event) {
+    event.target.style.opacity = 1;
+  });
+
+  $('#mainPage1').append(progressbar);
+  shapes[index] = progressbar;
+  index++;
+
+  console.log(shapes);
+}
+
+//Symbol Set mouse down event handler: To create new image
+function symbolsetMouseDownEventHandler(event) {
+  var leftOffset = document.getElementById('mainPage1').getBoundingClientRect().left;
+  var topOffset = document.getElementById('mainPage1').getBoundingClientRect().top;
+
+  var left = event.pageX - leftOffset + 'px';
+  var top = event.pageY - topOffset + 'px';
+
+  //Declare new image
+  var defaultSymbolSet = '../public/img/symbol-set/light-off.png';
+  var symbolSet = document.createElement('img');
+  symbolSet.id = 'symbolSet' + index;
+
+  //Image css style
+  symbolSet.src = defaultSymbolSet;
+  symbolSet.style.height = '50px';
+  symbolSet.style.width = '50px';
+  symbolSet.style.position = 'absolute';
+  symbolSet.style.top = top;
+  symbolSet.style.left = left;
+
+  //Image mouse events
+  $(symbolSet).on('mouseover', function (event) {
+    event.target.style.opacity = 0.4;
+    event.target.style.cursor = 'move';
+  });
+  //Subscribe mouseout event for each polygon
+  $(symbolSet).on('mouseout', function (event) {
+    event.target.style.opacity = 1;
+  });
+
+  $('#mainPage1').append(symbolSet);
+  shapes[index] = symbolSet;
+  index++;
+  
+  console.log(shapes);
+}
